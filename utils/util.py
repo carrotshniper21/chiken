5d269e2ec812708446a86ffce49ebecc205570d2
carrot <96284645+carrotshniper21@users.noreply.github.com>
2023-01-17 17:32:38 -0500

    Add files via upload

---
+++ utils/util.py
@@ -0,0 +1,17 @@
+"""
+Colorcodes for the cli to make the text look nicer lol
+"""
+
+colorcodes = {
+    "Black": "\033[30m",
+    "Red": "\033[31m",
+    "Green": "\033[32m",
+    "Yellow": "\033[33m",
+    "Blue": "\033[34m",
+    "Magenta": "\033[35m",
+    "Cyan": "\033[36m",
+    "White": "\033[37m",
+    "Reset": "\033[0m",
+    "Bold": "\033[1m",
+    "Gray": "\033[90m",
+}
